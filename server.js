3ad728a1fdaa5835c97d782e0dcf0bc0d2993448
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-02 14:50:07 -0400

    added initial comments for routes needed in server.js and added routes folder

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
+//home page
+//notes page
+// get /api/notes
+// get /api/notes/:id
+// post /api/notes
+// delete /api/notes
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
