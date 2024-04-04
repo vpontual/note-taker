30aec93499556b92e668dea67d217342834912dc
veepee <66334129+vpontual@users.noreply.github.com>
2024-04-03 20:04:55 -0400

    worked on index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const htmlRoutes = require("./htmlRoutes");
-const apiRoutes = require("./notesRoutes");
+const apiRoutes = require("./apiRoutes");
 
-router.use("/", htmlRoutes);
 router.use("/api", apiRoutes);
+router.use("/", htmlRoutes);
 
 module.exports = router;
